acdf2b53fb718ca225cfc6b394085406dead9ee3
yuhao900914 <yuhao900914@gmail.com>
2021-11-08 11:48:59 -0800

    implement init and logEevet function

--- lib/amplitude_web.dart
+++ lib/amplitude_web.dart
@@ -1,19 +1,9 @@
-@JS()
-
-import 'package:js/js.dart';
 import 'dart:convert';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'web/amplitude_js.dart';
 import 'dart:async';
-
-@JS('amplitude')
-class Amplitude {
-  external Amplitude(String instanceName);
-  external static Amplitude getInstance(String instanceName);
-  external void init(String api, String? userId, Object? option);
-  external void logEvent(String eventType);
-}
 
 class AmplitudeFlutterPlugin {
   static void registerWith(Registrar registrar) {
@@ -33,20 +23,17 @@ class AmplitudeFlutterPlugin {
     var args = jsonDecode(call.arguments.toString());
     var instanceName = args['instanceName'];
     Amplitude amplitude = Amplitude.getInstance(instanceName);
-
     switch (call.method) {
-      // Init
       case "init":
         {
-          var apiKey = args['apiKey'];
-          amplitude.init(apiKey, null, {});
-          return true;
+          String apiKey = args['apiKey'];
+          return amplitude.init(apiKey, args['userId'] ?? null);
         }
       case "logEvent":
         {
-          var eventType = args['eventType'];
-          amplitude.logEvent(eventType);
-          return true;
+          String eventType = args['eventType'];
+          return amplitude.logEvent(eventType, args['eventProperties'] ?? null);
+          //TODO: support outOfSession
         }
       default:
         throw PlatformException(
