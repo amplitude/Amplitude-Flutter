57f1c4ec968b770a24917bbf7186f3e07da42c17
yuhao900914 <yuhao900914@gmail.com>
2022-10-20 13:01:55 -0700

    refactor: example migration to Android embedding v2

--- example/android/app/src/main/kotlin/com/example/amplitude_flutter_example/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.example.amplitude_flutter_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-  // You can keep this empty class or remove it. Plugins on the new embedding
-  // now automatically registers plugins.
-}
