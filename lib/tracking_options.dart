66e51d50549b36838d010e46a9b2a0c20ef9cfa0
Xinyi Ye <xinyi.ye@amplitude.com>
2023-12-18 09:13:05 -0800

    fix typo

--- lib/tracking_options.dart
+++ lib/tracking_options.dart
@@ -8,100 +8,100 @@ class TrackingOptions {
   Set<String> _disabledFields = {};
 
   // Available on iOS, Android, and Web
-  TrackingOptions diableIpAddress() {
+  TrackingOptions disableIpAddress() {
     _disabledFields.add(Constants.ampTrackingOptionIpAddress);
     return this;
   }
 
-  TrackingOptions diableLanguage() {
+  TrackingOptions disableLanguage() {
     _disabledFields.add(Constants.ampTrackingOptionLanguage);
     return this;
   }
 
-  TrackingOptions diablePlatform() {
+  TrackingOptions disablePlatform() {
     _disabledFields.add(Constants.ampTrackingOptionPlatform);
     return this;
   }
 
   // Only avaliable on iOS and Android
-  TrackingOptions diableVersionName() {
+  TrackingOptions disableVersionName() {
     _disabledFields.add(Constants.ampTrackingOptionVersionName);
     return this;
   }
 
-  TrackingOptions diableOsName() {
+  TrackingOptions disableOsName() {
     _disabledFields.add(Constants.ampTrackingOptionOsName);
     return this;
   }
 
-  TrackingOptions diableOsVersion() {
+  TrackingOptions disableOsVersion() {
     _disabledFields.add(Constants.ampTrackingOptionOsVersion);
     return this;
   }
 
-  TrackingOptions diableDeviceManufacturer() {
+  TrackingOptions disableDeviceManufacturer() {
     _disabledFields.add(Constants.ampTrackingOptionDeviceManufacturer);
     return this;
   }
 
-  TrackingOptions diableDeviceModel() {
+  TrackingOptions disableDeviceModel() {
     _disabledFields.add(Constants.ampTrackingOptionDeviceModel);
     return this;
   }
 
-  TrackingOptions diableCarrier() {
+  TrackingOptions disableCarrier() {
     _disabledFields.add(Constants.ampTrackingOptionCarrier);
     return this;
   }
 
-  TrackingOptions diableCity() {
+  TrackingOptions disableCity() {
     _disabledFields.add(Constants.ampTrackingOptionCity);
     return this;
   }
 
-  TrackingOptions diableCountry() {
+  TrackingOptions disableCountry() {
     _disabledFields.add(Constants.ampTrackingOptionCountry);
     return this;
   }
 
-  TrackingOptions diableDma() {
+  TrackingOptions disableDma() {
     _disabledFields.add(Constants.ampTrackingOptionDma);
     return this;
   }
 
-  TrackingOptions diableRegion() {
+  TrackingOptions disableRegion() {
     _disabledFields.add(Constants.ampTrackingOptionRegion);
     return this;
   }
 
   // Only avaliable on Android
-  TrackingOptions diableADID() {
+  TrackingOptions disableADID() {
     _disabledFields.add(Constants.ampTrackingOptionAdid);
     return this;
   }
 
-  TrackingOptions diableAppSetId() {
+  TrackingOptions disableAppSetId() {
     _disabledFields.add(Constants.ampTrackingOptionAppSetId);
     return this;
   }
 
-  TrackingOptions diableDeviceBrand() {
+  TrackingOptions disableDeviceBrand() {
     _disabledFields.add(Constants.ampTrackingOptionDeviceBrand);
     return this;
   }
 
-  TrackingOptions diableLatLag() {
+  TrackingOptions disableLatLag() {
     _disabledFields.add(Constants.ampTrackingOptionLatLag);
     return this;
   }
 
-  TrackingOptions diableApiLevel() {
+  TrackingOptions disableApiLevel() {
     _disabledFields.add(Constants.ampTrackingOptionApiLevel);
     return this;
   }
 
   // Only available on Swift
-  TrackingOptions diableIDFV() {
+  TrackingOptions disableIDFV() {
     _disabledFields.add(Constants.ampTrackingOptionIdfv);
     return this;
   }
