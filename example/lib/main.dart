5c4ad5998af32fb46d826192f946b66e3bc03d82
Xinyi Ye <xinyi.ye@amplitude.com>
2024-02-28 15:03:05 -0800

    nit

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 import 'my_app.dart';
 
-void main() => runApp(const MyApp('5b9a9510e261f9ead90865bbc5a7ad1d'));
+void main() => runApp(const MyApp('API_KEY'));
