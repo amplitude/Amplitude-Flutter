cf17c661d4389f307991d7c3239d2b54d71c7a4b
Xinyi Ye <xinyi.ye@amplitude.com>
2024-03-12 15:38:14 -0700

    chore: should use single quote marks

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,15 +1,15 @@
 class Constants {
-  static const packageName = "amplitude-flutter";
-  static const packageVersion = "3.16.1";
-  static const identify_event = "\$identify";
-  static const group_identify_event = "\$groupidentify";
-  static const revenue_event = "revenue_amount";
+  static const packageName = 'amplitude-flutter';
+  static const packageVersion = '3.16.1';
+  static const identify_event = '\$identify';
+  static const group_identify_event = '\$groupidentify';
+  static const revenue_event = 'revenue_amount';
 
   static const flushQueueSize = 30;
   static const flushIntervalMillis = 30 * 1000; // 30 seconds
   static const identifyBatchIntervalMillis = 30 * 1000; // 30 seconds
-  static const defaultInstanceName = "\$default_instance";
-  static const logLevel = "info";
+  static const defaultInstanceName = '\$default_instance';
+  static const logLevel = 'info';
   static const flushMaxRetries = 5;
   static const minTimeBetweenSessionsMillis = 5 * 60 * 1000; // 5 minutes
 }
