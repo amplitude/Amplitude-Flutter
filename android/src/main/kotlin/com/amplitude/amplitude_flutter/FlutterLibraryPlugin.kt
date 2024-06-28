9f84fc42a114a37b31fb401b74df0d38baf6b8d8
Xinyi Ye <xinyi.ye@amplitude.com>
2024-06-28 10:11:11 -0700

    fix: Adding underlying library version to library field

--- android/src/main/kotlin/com/amplitude/amplitude_flutter/FlutterLibraryPlugin.kt
+++ android/src/main/kotlin/com/amplitude/amplitude_flutter/FlutterLibraryPlugin.kt
@@ -5,14 +5,14 @@ import com.amplitude.core.events.BaseEvent
 import com.amplitude.core.platform.Plugin
 
 class FlutterLibraryPlugin(val library: String): Plugin {
-    override val type: Plugin.Type = Plugin.Type.Before
+    override val type: Plugin.Type = Plugin.Type.Enrichment
     override lateinit var amplitude: Amplitude
 
     override fun execute(event: BaseEvent): BaseEvent? {
         if (event.library == null) {
             event.library = library
         } else {
-            event.library = "$library_${event.library}"
+            event.library = "${library}_${event.library}"
         }
         return super.execute(event)
     }
