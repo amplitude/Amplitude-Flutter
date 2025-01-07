1c7a8e7258cf6991836503ab3054cd84d73fb3c6
chungdaniel <daniel.chung@amplitude.com>
2025-01-07 10:43:19 -0800

    remove test key

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,4 @@ import 'package:flutter/material.dart';
 
 import 'my_app.dart';
 
-// web
-void main() => runApp(const MyApp('78617add4584f6ccf02463dba70eb842'));
-// ios
-// void main() => runApp(const MyApp('3a56c7789a57fbe54ff55f5cd63e0174'));
+void main() => runApp(const MyApp('API_KEY'));
