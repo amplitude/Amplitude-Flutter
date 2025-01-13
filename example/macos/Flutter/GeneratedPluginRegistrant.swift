8267006855fa156ebf5058928f60c73150c9ac42
chungdaniel <daniel.chung@amplitude.com>
2025-01-13 11:13:23 -0800

    add macos example

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import amplitude_flutter
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AmplitudeFlutterPlugin.register(with: registry.registrar(forPlugin: "AmplitudeFlutterPlugin"))
+}
